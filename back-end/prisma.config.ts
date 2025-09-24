278ef53975807a693ae97ac523cb83b10787248e
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-09-24 10:31:34 -0700

    Configuración correcta con entidades modulares

--- back-end/prisma.config.ts
+++ back-end/prisma.config.ts
@@ -1,9 +1,8 @@
 import path from 'node:path'
-// Carga las variables de entorno (.env)
 import 'dotenv/config'
 import type { PrismaConfig } from 'prisma'
 
 // Es usado para que el CLI de Prisma sepa donde está el schema
 export default {
-    schema: path.join(__dirname, 'prisma', 'schema', 'schema.prisma'),
+    schema: path.join(__dirname, 'prisma', 'schema'),
 } satisfies PrismaConfig
--- back-end/src/prisma/prisma.service.ts
+++ back-end/src/prisma/prisma.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     // Conexión a la base de datos al iniciar el módulo
     async onModuleInit() {
         await this.$connect();
