4850cf70cc478d11aab199ff8a40bc0a5c973935
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-09-24 20:52:51 -0700

    Setup front-end

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Main } from './main/main';
+import { Aside } from './aside/aside';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Main, Aside],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
