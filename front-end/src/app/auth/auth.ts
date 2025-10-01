efd92593d86882d9fbc135a7fcccab543eb586ce
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-09-30 22:06:46 -0700

    side screen finished

--- front-end/src/app/auth/auth.ts
+++ front-end/src/app/auth/auth.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { IconTextButton } from "../main/icon-text-button/icon-text-button";
 
 @Component({
   selector: 'app-auth',
   imports: [RouterOutlet],
   templateUrl: './auth.html',
-  styleUrl: './auth.css'
+  styleUrl: './auth.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Auth {
+export class Auth implements OnInit, OnDestroy {
+  images = [
+    'assets/images/calendar.png',
+    'assets/images/pop-up.png',
+    'assets/images/tasks.png'
+  ];
 
+  currentIndex = 0;
+  private intervalId?: number;
+  private readonly INTERVAL_TIME = 5000;
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  ngOnDestroy() {
+    this.stopCarousel();
+  }
+
+  startCarousel() {
+    this.intervalId = window.setInterval(() => {
+      this.nextSlide();
+      this.cdr.markForCheck();
+    }, this.INTERVAL_TIME);
+  }
+
+  stopCarousel() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
 }
--- front-end/src/app/auth/register/register.ts
+++ front-end/src/app/auth/register/register.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { IconTextButton } from "../../main/icon-text-button/icon-text-button";
 import { AuthSection } from "../auth-section/auth-section";
 
 @Component({
   selector: 'app-register',
-  imports: [IconTextButton, AuthSection],
+  imports: [AuthSection],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
