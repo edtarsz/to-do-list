8d1a762306fa6db161bf23fc8a4eb77e0035243f
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-10-16 09:58:48 -0700

    url update

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    BACK_END_URL: 'https://to-do-list-back-end-ppq.onrender.com'
+    BACK_END_URL: 'https://to-do-list-back-end-pjoq.onrender.com'
 };
